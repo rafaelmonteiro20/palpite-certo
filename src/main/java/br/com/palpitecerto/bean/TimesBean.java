0da44d2261c5d5ff02c274a356a611f5c991fafe
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-05-31 19:24:09 -0300

    Configurando o ViewScoped

---
+++ src/main/java/br/com/palpitecerto/bean/TimesBean.java
@@ -0,0 +1,21 @@
+package br.com.palpitecerto.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class TimesBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	
+	
+}
