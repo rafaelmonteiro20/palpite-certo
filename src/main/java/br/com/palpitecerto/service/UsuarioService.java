0837b7bcf1561cedd92814bba50aba2feba9b911
josimarjunior <juniorpaz99@gmail.com>
2018-06-03 19:07:03 -0300

    Modelo e DAO do Usuário

---
+++ src/main/java/br/com/palpitecerto/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package br.com.palpitecerto.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import br.com.palpitecerto.dao.UsuarioDAO;
+import br.com.palpitecerto.infra.jpa.Transactional;
+import br.com.palpitecerto.model.Usuario;
+
+public class UsuarioService implements Serializable {
+
+	private static final long serialVersionUID = 5411157659288446766L;
+
+	@Inject
+	private UsuarioDAO usuarioDAO;
+	
+	public List<Usuario> listar() {
+		return usuarioDAO.listar();
+	}
+	
+	@Transactional
+	public void salvarOuAtualizar(Usuario usuario) {
+		usuarioDAO.salvarOuAtualizar(usuario);
+	}
+	
+	public Usuario getPorId(Long id) {
+		return usuarioDAO.getPorId(id);	
+	}
+	
+	public void remover(Usuario usuario) {
+		usuarioDAO.remover(usuario);
+	}
+}
