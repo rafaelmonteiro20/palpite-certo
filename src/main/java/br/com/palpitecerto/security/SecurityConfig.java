53b82563be937027ac179e10e86268f99a6883c7
Luiz Jacó <luizjaco@unicatolicaquixada.edu.br>
2018-06-08 18:20:30 -0300

    Cadastro de resultados e encerramento de rodada

--- src/main/java/br/com/palpitecerto/security/SecurityConfig.java
+++ src/main/java/br/com/palpitecerto/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.antMatchers("/login.xhtml", "/javax.faces.resource/**", "/meu-cadastro.xhtml").permitAll()
 				.antMatchers("/home.xhtml", "/acesso-negado.xhtml", "/pagina-nao-encontrada.xhtml").authenticated()
-				.antMatchers("/administrador/**", "/campeonatos.xhtml", "/rodadas.xhtml", "/times.xhtml").hasAnyRole("ADMINISTRADOR")
+				.antMatchers("/administrador/**", "/campeonatos.xhtml", "/rodadas.xhtml", "/times.xhtml", "/cadastrar-resultados.xhtml").hasAnyRole("ADMINISTRADOR")
 				.and()
 
 			.formLogin()
